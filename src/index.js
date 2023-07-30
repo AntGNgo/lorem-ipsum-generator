f8f424bdb095566312025ae368d37e54d0a77a56
Anthony Ngo <anthony.g.ngo@gmail.com>
2023-07-30 11:53:37 -0700

    change words to paragraphs

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const generate = document.getElementById('generate');
 
 generate.addEventListener('click', (e) => {
   e.preventDefault();
-  let generatedText = lorem.generateWords(parseInt(length.value));
+  let generatedText = lorem.generateParagraphs(parseInt(length.value));
   console.log(generatedText);
   text.value = generatedText;
 });
